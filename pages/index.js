b560002c85c6550c6ca8b3694c9612cdf75ead32
Chris Leensvaart <cleensvaart@gmail.com>
2022-05-15 18:53:05 -0500

    removed articles

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,12 @@
-import { server } from '../config';
 import Head from 'next/head';
-import ArticleList from '../components/ArticleList';
-import { articles } from '../data';
 
-export default function Home({ articles }) {
+export default function Home() {
   return (
     <div>
       <Head>
         <title>Tiny Orchard News</title>
         <meta name='keywords' content='homeschooling, dyslexia, tutor' />
       </Head>
-      <ArticleList articles={articles} />
     </div>
   );
 }
-
-export const getStaticProps = async () => {
-  const res = await fetch(`${server}/api/articles`);
-  const articles = await res.json();
-
-  return {
-    props: {
-      articles,
-    },
-  };
-};
-
-// export const getStaticProps = async () => {
-//   const res = await fetch(
-//     `https://jsonplaceholder.typicode.com/posts?_limit=6`
-//   );
-//   const articles = await res.json();
-
-//   return {
-//     props: {
-//       articles,
-//     },
-//   };
-// };
