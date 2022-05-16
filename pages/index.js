02f6dbea8067e7c4f4cfb4c6fdf9ca97b8df213b
Chris Leensvaart <cleensvaart@gmail.com>
2022-05-16 08:30:29 -0500

    Added ArticleList

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import Head from 'next/head';
+import ArticleList from '../components/ArticleList';
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title>Tiny Orchard</title>
         <meta name='keywords' content='homeschooling, dyslexia, tutor' />
       </Head>
-      <h1>Welcome to Tiny Orchard</h1>
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
