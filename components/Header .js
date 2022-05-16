7603c0c58262d7057a15798b312ccb56c5201945
Chris Leensvaart <cleensvaart@gmail.com>
2022-05-16 08:18:26 -0500

    Completed Header

--- components/Header .js
+++ components/Header .js
@@ -1,7 +1,14 @@
 import headerStyles from '../styles/Header.module.css';
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className={headerStyles.title}>
+        <span>Renee is Awesome!</span>
+      </h1>
+      <p className={headerStyles.description}>Learn stuff about things</p>
+    </div>
+  );
 };
 
 export default Header;
