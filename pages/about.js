ccb0c6b9242ec7a80ad50dc212a6d125c32de131
Chris Leensvaart <cleensvaart@gmail.com>
2022-05-16 22:16:55 -0500

    Added Meta info for head and head component.

--- pages/about.js
+++ pages/about.js
@@ -3,9 +3,6 @@ import Head from 'next/head';
 const about = () => {
   return (
     <div>
-      <Head>
-        <title>About Us</title>
-      </Head>
       <h1>About</h1>
     </div>
   );
--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,5 +1,6 @@
+import { server } from '../../../config';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import Meta from '../../../components/Meta';
 
 const article = ({ article }) => {
   // const router = useRouter();
@@ -7,6 +8,7 @@ const article = ({ article }) => {
 
   return (
     <>
+      <Meta title={article.title} description={article.excerpt} />
       <h1>{article.title}</h1>
       <p>{article.body}</p>
       <br />
@@ -16,9 +18,7 @@ const article = ({ article }) => {
 };
 
 export const getStaticProps = async (context) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-  );
+  const res = await fetch(`${server}/api/articles/${context.params.id}`);
 
   const article = await res.json();
 
@@ -30,13 +30,36 @@ export const getStaticProps = async (context) => {
 };
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const res = await fetch(`${server}/api/articles/`);
 
   const articles = await res.json();
 
   const ids = articles.map((article) => article.id);
 
   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  // export const getStaticProps = async (context) => {
+  //   const res = await fetch(
+  //     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  //   );
+
+  //   const article = await res.json();
+
+  //   return {
+  //     props: {
+  //       article,
+  //     },
+  //   };
+  // };
+
+  // export const getStaticPaths = async () => {
+  //   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+  //   const articles = await res.json();
+
+  //   const ids = articles.map((article) => article.id);
+
+  //   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
 
   return {
     paths,
